32b272c78e919dc3abdfb89b81a01f4325de0292
Kentaro Fujiy <kentaro@manacadigital.com.br>
2020-03-24 01:00:11 +0000

    setup

--- Angular-Basic-Video-Chat/src/config.ts
+++ Angular-Basic-Video-Chat/src/config.ts
@@ -1,11 +1,11 @@
 export default {
   // Set this to the base URL of your sample server, such as 'https://your-app-name.herokuapp.com'.
   // Do not include the trailing slash. See the README for more information:
-  SAMPLE_SERVER_BASE_URL: 'http://YOUR-SERVER-URL',
+  SAMPLE_SERVER_BASE_URL: 'https://4200-d17f8a2a-88d4-4d95-90e7-1dcbcaa556b0.ws-us02.gitpod.io',
   // OR, if you have not set up a web server that runs the learning-opentok-php code,
   // set these values to OpenTok API key, a valid session ID, and a token for the session.
   // For test purposes, you can obtain these from https://tokbox.com/account.
-  API_KEY: '',
-  SESSION_ID: '',
-  TOKEN: ''
+  API_KEY: '46601752',
+  SESSION_ID: '1_MX40NjYwMTc1Mn5-MTU4NTAwOTg1ODIwNH5SbVArMTB5djR6VkYveVREaXBuTGhYRkd-UH4',
+  TOKEN: 'T1==cGFydG5lcl9pZD00NjYwMTc1MiZzaWc9ZmJkN2E0N2Q0ZWMyZmQxZmU2ODc2ZWUwN2E3MGFhMWI3YmVmZDJjNTpzZXNzaW9uX2lkPTFfTVg0ME5qWXdNVGMxTW41LU1UVTROVEF3T1RnMU9ESXdOSDVTYlZBck1UQjVkalI2VmtZdmVWUkVhWEJ1VEdoWVJrZC1VSDQmY3JlYXRlX3RpbWU9MTU4NTAwOTkzOCZub25jZT0wLjE2ODY5MTUxOTUyNzg3MzEmcm9sZT1wdWJsaXNoZXImZXhwaXJlX3RpbWU9MTU4NTYxNDczOCZpbml0aWFsX2xheW91dF9jbGFzc19saXN0PQ=='
 };
